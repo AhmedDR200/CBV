141a74acbb5f3ef99c26ea7df6b8ce9da82a4d79
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-05 20:46:10 +0300

    Admin panal customization

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Post
+
 # Register your models here.
 
+admin.site.site_header = "Blog Admin Panal"
+admin.site.site_title = "Blog Admin Panal"
 
 
-admin.site.register(Post)
+class TopicAdmin(admin.ModelAdmin):
+    fields= ("title","content")
+    list_display=["id",  'title' , 'active']
+    list_editable = ('title',)
+    search_fields=['title','content']
+    list_filter = ('created_at',)
+    date_hierarchy='created_at'
+    
+
+    
+
+
+
+
+admin.site.register(Post , TopicAdmin)
+
+
--- post/apps.py
+++ post/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
 
 
 class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'post'
+
+
+
+class SuitConfig(DjangoSuitConfig):
+    layout = "vertical"
